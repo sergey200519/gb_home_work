c85e281b501007078762dbafa211eea6e8457a89
sergey <oborok05@bk.ru>
2023-02-19 15:31:22 +0300

    lesson5-fix

--- java_dz/lesson5/prog1.java
+++ java_dz/lesson5/prog1.java
@@ -1,13 +1,21 @@
 package java_dz.lesson5;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class prog1 {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         HashMap<Integer, String> phone = new HashMap<>();
-        phone.put(89753559, "ivan");
-        phone.put(96523467, "max");
+        while (true) {
+            System.out.println("--------------------------------");
+            int num = scanner.nextInt();
+            String name = scanner.next();
+            if (num == 0) break;   
+            phone.put(num, name);
+        }
         System.out.println(phone);
         System.out.println(phone.get(89753559));
+        scanner.close();
     }
 }
---
+++ java_dz/lesson6/prog1.java
@@ -0,0 +1,5 @@
+package java_dz.lesson6;
+
+public class prog1 {
+    
+}
